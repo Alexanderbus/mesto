fa0e3e89b4a432071ae57fa0d0c03bbb13e30b79
Alexander Buslaev <alexandrobuslaev@gmail.com>
2023-03-27 14:07:46 +0300

    fix 2 review

--- scripts/Profile.js
+++ scripts/Profile.js
@@ -1,33 +1,29 @@
 let editProfileBtn = document.querySelector('.profile__button');
 let popup = document.querySelector('.pop-up');
 let editor = document.editor;
-let name = editor.name;
+let myname = editor.name;
 let about = editor.about;
-let whoau = document.querySelector('.profile__whoau');
-let nickname = document.querySelector('.profile__name');
-let changheprofile = document.querySelector('.pop-up__edit');
-let editheader = document.querySelector('.pop-up__header');
-let closebutton = document.querySelector('.pop-up__exit');
+let whoAU = document.querySelector('.profile__whoau');
+let nickName = document.querySelector('.profile__name');
+let closeBtn = document.querySelector('.pop-up__exit');
 
 //открытие поп апа
 editProfileBtn.addEventListener('click', function() {
     popup.classList.add('pop-up_opened');
-    name.setAttribute("placeholder", nickname.textContent);
-    about.setAttribute("placeholder", whoau.textContent);
+    myname.value = nickName.textContent;
+    about.value = whoAU.textContent;
 })
 
 //функция по редактированию профиля
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    if (name.value.length > 0 && about.value.length > 0) {
-    nickname.textContent = name.value;
-    whoau.textContent = about.value;
+    if (myname.value.length > 0 && about.value.length > 0) {
+    nickName.textContent = myname.value;
+    whoAU.textContent = about.value;
     popup.classList.remove('pop-up_opened');
-    name.value = '';
-    about.value = ''; 
 } else {
-    name.setAttribute("placeholder", 'Введите имя!');
-    about.setAttribute("placeholder", 'Введите кто вы!');
+    myname.setAttribute("placeholder", 'Введите имя!');
+    about.setAttribute("placeholder", 'Введите информацию о себе!');
 }
 }
 
@@ -35,6 +31,6 @@ function handleFormSubmit (evt) {
 editor.addEventListener('submit', handleFormSubmit);
 
 //кнопка закрыть попап
-closebutton.addEventListener('click',  function(){ 
+closeBtn.addEventListener('click',  function(){ 
     popup.classList.remove('pop-up_opened');
 })
