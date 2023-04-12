2032ff1e36e9e46ff143d9b3d7d0d3a9eed266a9
Alexander Buslaev <alexandrobuslaev@gmail.com>
2023-04-12 09:05:06 +0300

    fix 4 rewiev

--- scripts/Profile.js
+++ scripts/Profile.js
@@ -1,7 +1,7 @@
 let editProfileBtn = document.querySelector('.profile__button');
 let popup = document.querySelector('.pop-up');
 let editor = document.editor;
-let myname = editor.name;
+let myName = editor.name;
 let about = editor.about;
 let whoAU = document.querySelector('.profile__whoau');
 let nickName = document.querySelector('.profile__name');
@@ -10,19 +10,19 @@ let closeBtn = document.querySelector('.pop-up__exit');
 //открытие поп апа
 editProfileBtn.addEventListener('click', function () {
     popup.classList.add('pop-up_opened');
-    myname.value = nickName.textContent;
+    myName.value = nickName.textContent;
     about.value = whoAU.textContent;
 })
 
 //функция по редактированию профиля
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    if (myname.value.length > 0 && about.value.length > 0) {
-        nickName.textContent = myname.value;
+    if (myName.value.length > 0 && about.value.length > 0) {
+        nickName.textContent = myName.value;
         whoAU.textContent = about.value;
         popup.classList.remove('pop-up_opened');
     } else {
-        myname.setAttribute("placeholder", 'Введите имя!');
+        myName.setAttribute("placeholder", 'Введите имя!');
         about.setAttribute("placeholder", 'Введите информацию о себе!');
     }
 }
