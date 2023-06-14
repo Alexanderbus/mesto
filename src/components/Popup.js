ff43293abef31958581bb368b0590104ccae2a0f
Alexander Buslaev <alexandrobuslaev@gmail.com>
2023-06-14 14:41:20 +0300

    please work

---
+++ src/components/Popup.js
@@ -0,0 +1,33 @@
+export class Popup {
+    constructor(popup) {
+        this._popup = popup;
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+    
+        open(){
+            this._popup.classList.add('popup_opened')
+            document.addEventListener('keydown', this._handleEscClose)
+            console.log('охуеть')
+        }
+
+        close() {
+            this._popup.classList.remove('popup_opened')
+            document.removeEventListener('keydown', this._handleEscClose)
+        }
+
+        _handleEscClose(event) {
+            if (event.key === 'Escape') {
+              this.close();
+              console.log('охуеть-опупеть')
+            }
+        }
+
+        setEventListeners() {
+            this._popup.addEventListener('click', (event) => {
+              if (event.target.classList.contains('popup') || event.target.classList.contains('popup__exit')) {
+                this.close();
+                console.log('охуеть-опупеть-гремучий лес')
+              }
+            });
+          } 
+}
